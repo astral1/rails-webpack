0b15dfd47667563fb58ef99edab2f59689e7eb44
Jeong, Jiung <ethernuiel@sanultari.com>
2015-10-28 16:24:38 +0900

    split processor and dependency_collection

--- lib/rails/webpack.rb
+++ lib/rails/webpack.rb
@@ -3,6 +3,7 @@ module Rails
     require 'rails/webpack/version'
     require 'rails/webpack/railtie'
     autoload :DependencyCollection, 'rails/webpack/dependency_collection'
+    autoload :Processor, 'rails/webpack/processor'
     autoload :Config, 'rails/webpack/config'
     autoload :Actions, 'rails/webpack/actions'
   end
